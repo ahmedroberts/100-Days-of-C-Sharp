eb990d3aca0ad7ad0bc33a2e3bf647ea274089a5
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-30 19:00:47 -0500

    Add MS C# challenge code.

--- ConsoleBasicApp/Program.cs
+++ ConsoleBasicApp/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp001
             // Watching YouTubes: https://www.youtube.com/watch?v=M5ugY7fWydE Derek Banas
             // & https://www.youtube.com/watch?v=0QUgvfuKvWU Microsoft Developer
             // $ https://www.youtube.com/watch?v=wxznTygnRfQ Bro Code
+            // Also Microsoft C# New Year | New Certification C#
             //Console.ForegroundColor = ConsoleColor.DarkGreen;
             //Console.BackgroundColor = ConsoleColor.DarkBlue;
             //Console.Clear();
@@ -16,10 +17,45 @@ namespace ConsoleApp001
             Console.WriteLine("    Jumbo, Rafiki\n    Raikage Training");
             Console.WriteLine("\n***********************************\n");
 
+            string project1Name = "First-Project";
+            Console.WriteLine($@"C:\Output\{project1Name}\Data");
+
             //Console.WriteLine("What is your name?");
             //Console.ForegroundColor = ConsoleColor.Magenta;
             //string? userName = Console.ReadLine();
             //Console.WriteLine($"Ohayo, {userName}!");
+
+            Console.WriteLine("c:\\source\\repos");
+            Console.WriteLine(@"    c:\source\repos    
+                    (this is where your code goes)");
+            // Kon'nichiwa World
+            // **Nihongo**
+            Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+            Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+            Console.WriteLine("Invoice: 1021\t\tComplete!");
+            Console.WriteLine("Invoice: 1022\t\tComplete!");
+            Console.WriteLine("\nOutput Directory:\t");
+            Console.Write(@"c:\invoices");
+
+            // To generate Japanese invoices:
+            // Nihon no seikyū-sho o seisei suru ni wa:
+            // **Nihongo**
+            Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+            // User command to run an application
+            Console.WriteLine(@"c:\invoices\app.exe -j");
+
+            string firstName = "Bob";
+            string message = $"Hello {firstName}!";
+            Console.WriteLine(message);
+
+            string projectName = "ACME";
+            string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+            Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+            string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+            Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
             // Variables
             /*
