b6402d8e4b618ea72080a7c4563ae147f68ea22e
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-01-31 19:23:15 -0500

    Add perform basic operations on numbers

--- ConsoleBasicApp/Program.cs
+++ ConsoleBasicApp/Program.cs
@@ -92,7 +92,19 @@ namespace ConsoleApp001
             Console.WriteLine("\n\nAhmed's Big Giveaway");
             Console.WriteLine("Choose a door: 1, 2, or 3: ");
             string? userValue = Console.ReadLine();
+            */
 
+            int sum = 7 + 5;
+            int difference = 7 - 5;
+            int product = 7 * 5;
+            int quotient = 7 / 5;
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + difference);
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Quotient: " + quotient);
+
+            /*
             string? prizeMessage = "";
             if (userValue == "1")
                 prizeMessage = "You won a cupcake!";
@@ -115,6 +127,45 @@ namespace ConsoleApp001
             Console.Write(prizeMessage);
             Console.Write("!");
             Console.ReadLine();
+
+            int value = 0;     // value is now 0.
+            value = value + 5; // value is now 5.
+            value += 5;        // value is now 10.
+
+            value = 0;     // value is now 0.
+            value = value + 1; // value is now 1.
+            value++;           // value is now 2.
+
+            value = 1;
+
+            value = value + 1;
+            Console.WriteLine("First increment: " + value);
+
+            value += 1;
+            Console.WriteLine("Second increment: " + value);
+
+            value++;
+            Console.WriteLine("Third increment: " + value);
+
+            value = value - 1;
+            Console.WriteLine("First decrement: " + value);
+
+            value -= 1;
+            Console.WriteLine("Second decrement: " + value);
+
+            value--;
+            Console.WriteLine("Third decrement: " + value);
+
+            value = 1;
+            value++;
+            Console.WriteLine("First: " + value);
+            Console.WriteLine($"Second: {value++}");
+            Console.WriteLine("Third: " + value);
+            Console.WriteLine("Fourth: " + (++value));
+
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+            Console.WriteLine("The temperature is " + celsius + " Celsius.");
 
         }
     }
